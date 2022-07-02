641b7168cf33c0fbcf4502fa60d9f2ab37db58e7
AlexSabur <alexsabur@live.ru>
2022-07-02 00:44:49 +0000

    update logging

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,10 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => [
+                'daily',
+                'stderr'
+            ],
             'ignore_exceptions' => false,
         ],
 
@@ -67,7 +70,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 7,
         ],
 
         'slack' => [
