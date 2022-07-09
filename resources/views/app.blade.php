5d3ece1fd38cd692d7624d30b36d37446fffc29d
Alex <alexsabur@live.ru>
2022-07-09 10:58:04 +0000

    шаблон приложения

---
+++ resources/views/app.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <script>
+        __info__ = {
+            user: @js($user),
+            token: `{{ $token }}`,
+            rpc: `{{ route('rpc.endpoint', absolute: true) }}`,
+        };
+
+        __routes__ = {
+            replaceUrl(url, data) {
+                const regex = new RegExp(':(' + Object.keys(data).join('|') + ')', 'g');
+
+                return url.replace(regex, (m, $1) => data[$1] || m);
+            },
+            reportDownload({ id }) {
+                return this.replaceUrl(`{{{ route('report.download', ['report' => ':id']) }}}`, { id })
+            }
+        }
+    </script>
+    <div id="pwa">
+        <router-view></router-view>
+    </div>
+    <script>
+        if ('serviceWorker' in navigator ) {
+
+            // window.addEventListener('load', async () => {
+            //     try {
+            //         const registration = await navigator.serviceWorker.register('/service-worker.js')
+
+            //         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            //     } catch (error) {
+            //         console.error('ServiceWorker registration failed: ', err);
+            //     }
+            // });
+        }
+</script>
+@endsection
